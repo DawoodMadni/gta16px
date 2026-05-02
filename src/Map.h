64a1557bd215ee261708c8927f183dacb21bb0a0
DawoodMadni <l250719@lhr.nu.edu.pk>
2026-05-02 23:37:49 +0500

    Update Map.h

--- src/Map.h
+++ src/Map.h
@@ -1,6 +1,6 @@
 #pragma once
-
 #include <iostream>
+using namespace std;
 #include <SFML/Graphics.hpp>
 
 class Map
@@ -24,3 +24,54 @@ private:
     // midground tiles
     // foreground tiles
 };
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "RPG");
+    // window.setFramerateLimit(60);
+    sf::Texture texture;
+    if (!texture.loadFromFile("Player/Assets/playerspritesheet.png"))
+    {
+        cout << "Failed\n";
+        return -1;
+    }
+    sf::Sprite sprite(texture);
+    cout << "loaded\n";
+    sprite.setTextureRect(sf::IntRect({ 0, 3*145 }, {85, 145}));
+    sprite.scale(sf::Vector2f(1, 1));
+
+    sf::Clock timemeasure;
+    while (window.isOpen())
+    {
+        float dt = timemeasure.restart().asSeconds();
+        while (const auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            sprite.setTextureRect(sf::IntRect({ 0, 3*145 }, {85, 145}));
+            sprite.move({0.f,-200.f *dt});
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            sprite.setTextureRect(sf::IntRect({ 0, 0*145 }, {85, 145}));
+            sprite.move({0.f,200.f *dt});
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            sprite.setTextureRect(sf::IntRect({ 0, 2*145 }, {85, 145}));
+            sprite.move({200.f *dt,0.f});
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            sprite.setTextureRect(sf::IntRect({ 0, 1*145 }, {85, 145}));
+            sprite.move({-200.f *dt,0.f});
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+    return 0;
+}
